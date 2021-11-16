c06357bd273272808f70c3ea8bf22a0c6253cb76
EnderKingJ <78042486+EnderKingJ@users.noreply.github.com>
2021-11-15 23:24:36 -0500

    Update main.cr

--- main.cr
+++ main.cr
@@ -3,7 +3,7 @@ require "http/client"
 require "uri"
 
 server = HTTP::Server.new do |context|
-    HTTP::Client.options(context.request.path.lchop('/'), headers: context.request.headers) do |response|
+    HTTP::Client.request(context.request.path.lchop('/'), headers: context.request.headers) do |response|
         context.response.status_code = response.status_code
         case response.headers["Content-Type"]
         when "text/html"
