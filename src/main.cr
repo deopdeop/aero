5d5cce44de6b5ecd179b015203aa30a69b72f0a5
deopdeop <45722321+deopdeop@users.noreply.github.com>
2021-11-16 23:46:45 -0500

    Finish http proxying

--- src/main.cr
+++ src/main.cr
@@ -1,15 +1,40 @@
 require "http/server"
 require "http/client"
+require "json"
 require "base64"
 
 server = HTTP::Server.new do |context|
-  HTTP::Client.options(context.request.path.lchop('/')) do |response|
+  # TODO: Convert to macro
+  def rewrite_url(url)
+    return context.request.headers["host"] + '/' + url
+  end
+
+  request_uri = context.request.path.lchop('/')
+  request_url = req_uri.lchop("http").lchop('s').lchop("://")
+  request_host = req_url.split('/').first?
+
+  context.request.headers.host = request_host
+  context.request.headers.location = rewrite_url(context.request.headers.location)
+
+  HTTP::Client.options(url, {headers: context.request.headers}) do |response|
+    cors = Hash.new
+    response.headers.each do |key, value|
+      # TODO: Add more header keys
+      if key.in? "content-type", "x-frame-options"
+        cors[key] = value
+        next
+      end
+      context.response.headers[key] = value
+    end
+
     context.response.status_code = response.status_code
-    case response.headers["Content-Type"].split(';')[0]
+
+    # TODO: Add more deprecated content types
+    case response.headers["content-type"].split(';').first?
     when "text/html"
       body = "
         <script>
-            let cors;
+            let cors = #{cors.to_json};
 
             #{File.read("_window.js")}
 
@@ -24,7 +49,11 @@ server = HTTP::Server.new do |context|
             #{response.body}
         }({ ...window, ..._window }));
         "
-    when "application/json"
+    when "application/manifest+json"
+      json = JSON.parse(response.body)
+      puts json
+      # TODO: Implement manifest rewriting
+      body = json.to_json
     end
     puts body
     context.response.print body
