d33b12301cc65ec74cad558fd2129863bdb22b4e
deopdeop <45722321+deopdeop@users.noreply.github.com>
2021-11-17 10:22:48 -0500

    Update main.cr
    Compile time optimizations, added more rewrites, and added https

--- src/main.cr
+++ src/main.cr
@@ -1,26 +1,34 @@
 require "http/server"
 require "http/client"
+require "socket"
+require "openssl"
 require "json"
 require "base64"
 
-server = HTTP::Server.new do |context|
-  # TODO: Convert to macro
-  def rewrite_url(url)
-    return context.request.headers["host"] + '/' + url
-  end
+macro rewrite_url(url)
+  "#{context.request.headers["host"]}/#{{{url}}}"
+end
+macro rewrite_cookie(cookie)
+  {{cookie}}
+end
 
+server = HTTP::Server.new do |context|
   request_uri = context.request.path.lchop('/')
+  request_origin = request_uri.split('/')[2]
   request_url = req_uri.lchop("http").lchop('s').lchop("://")
-  request_host = req_url.split('/').first?
+  request_host = req_origin.first?
 
   context.request.headers.host = request_host
   context.request.headers.location = rewrite_url(context.request.headers.location)
 
   HTTP::Client.options(url, {headers: context.request.headers}) do |response|
     cors = Hash.new
+    
+    response.set_cookie = rewrite_cookie(response.set_cookie)
+    response.set_cookie2 = rewrite_cookie(response.set_cookie2)
+    response.origin = request_origin
     response.headers.each do |key, value|
-      # TODO: Add more header keys
-      if key.in? "content-type", "x-frame-options"
+      if key.in? "content-encoding", "timing-allow-origin", "x-frame-options"
         cors[key] = value
         next
       end
@@ -29,15 +37,14 @@ server = HTTP::Server.new do |context|
 
     context.response.status_code = response.status_code
 
-    # TODO: Add more deprecated content types
     case response.headers["content-type"].split(';').first?
-    when "text/html"
+    when "text/html" || "text/x-html"
       body = "
         <script>
             let cors = #{cors.to_json};
 
             #{File.read("_window.js")}
-
+    
             document.write(atob('#{Base64.strict_encode(response.body)}'));
         </script>
         "
@@ -52,7 +59,7 @@ server = HTTP::Server.new do |context|
     when "application/manifest+json"
       json = JSON.parse(response.body)
       puts json
-      # TODO: Implement manifest rewriting
+      # TODO: Rewrite
       body = json.to_json
     end
     puts body
@@ -60,4 +67,8 @@ server = HTTP::Server.new do |context|
   end
 end
 
-server.listen(80)
+ssl = OpenSSL::SSL::Context::Server.new
+ssl.certificate_chain = "ssl/public.cert"
+ssl.private_key = "ssl/private.key"
+
+server.bind_tls "127.0.0.1", 8080, context
