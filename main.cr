ecb269e1cd0f9e80bcb0456f6ccc16cf8539726a
deopdeop <45722321+deopdeop@users.noreply.github.com>
2021-12-12 00:36:24 -0500

    Update main.cr

--- main.cr
+++ main.cr
@@ -27,10 +27,10 @@ server = HTTP::Server.new([
   folder = "static/"
   case context.request.path.lchop('/'))
   when "/"
-    File.read("#{folder}/index.html}")
+    File.read("#{folder}/index.html")
     next
   when "/sw.js"
-    File.read("#{folder}/sw.js}")
+    File.read("#{folder}/sw.js")
     next
   end
 
