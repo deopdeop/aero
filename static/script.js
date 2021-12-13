533a2d01ae3cef6c97c8fe5f9e8444329d2f27da
Degenerate0001 <brettadams314@protonmail.com>
2021-12-13 00:06:29 -0600

    Rough New Frontend Stuff
    Nebelung is doing some sort of settings thing that is complicated and the current HTML that includes it isn't ready to be committed. I need to simplify and clean everything up before this is ready for public use.

---
+++ static/script.js
@@ -0,0 +1,54 @@
+const go = () => {
+  const redirectTo = url => window.location.href = `/${btoa(url)}`;
+
+  const url = document.getElementById('search').value;
+  if (url !== '') {
+    if (url.includes('.') && !url.includes(' '))
+      redirectTo(url.substring(0, 4) === 'http' ? url : 'https://' + url);
+    else if (url.includes('.') && !url.includes(' '))
+      redirectTo(url.substring(0, 4) === 'http' ? url : 'https://' + url);
+    else
+      redirectTo(`https://search.brave.com/search?q=${url.replace(/ /g, '+')}`);
+  }
+
+  window.addEventListener("load", function() {
+    document.querySelector(".go").addEventListener("click", go);
+    const search = document.getElementById("search");
+    search.addEventListener("keyup", event => {
+      event.preventDefault();
+      event.keyCode == 13 && search.value !== '' && go();
+    });
+  });
+};
+/*Settings Stuff | Overcomplicated and Doesn't Work
+
+const $ = document.querySelector.bind(document)
+const tc = () => {
+ localStorage.setItem("tc",localStorage.getItem("tc") ? "" : "true")
+ handleToggle()
+}
+const handleToggle = () => {
+  const tc = localStorage.getItem("tc")
+    const tcclass = $(".istc").classList
+    tcclass.remove(tc ? "disabled" : "enabled")
+    tcclass.add(tc ? "enabled" : "disabled")
+    $(".istc").innerHTML = tc ? "enabled" : "disabled"
+    
+}
+let tabCloaking = localStorage.getItem("tc") || ""
+handleToggle()
+window.addEventListener("load", function() {
+$(".settings").addEventListener("click", ()=>{
+  $("#settings-tab").style.display = "block"
+})
+$("#tc").addEventListener("click",tc)
+$(".go").addEventListener("click",go)
+var search = document.getElementById("search")
+search.addEventListener("keyup", function(event) {
+    event.preventDefault()
+    if (event.keyCode == 13)
+        if (search.value !== "") {
+             go()
+        }
+    })
+});*/
