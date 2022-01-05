87c9ceb19a07d5adc52426e9d5e914c8ff126690
deopdeop <45722321+deopdeop@users.noreply.github.com>
2022-01-05 12:20:28 -0500

    Fix scoping

--- examples/basic/static/utils.js
+++ examples/basic/static/utils.js
@@ -1,4 +1,4 @@
-function rewriteUrl(origin) {
+function rewriteUrl(url, origin) {
     if (url.startsWith('data:'))
         return url;
     else if (url.startsWith('./'))
@@ -6,7 +6,7 @@ function rewriteUrl(origin) {
 
     const validProtocol = url.startsWith('http://') || url.startsWith('https://') || url.startsWith('ws://') || url.startsWith('wss://');
 
-    const rewritten = prefix + (validProtocol ? url : origin + url);
+    const rewritten = prefix + (validProtocol ? url : ctx.url.origin + url);
 
     console.log(rewritten);
 
@@ -16,8 +16,15 @@ function rewriteUrl(origin) {
 function scope(script) {
     return `
 	{
+		import '/window.js'
+		
+		const window = new Proxy({}, {
+			get(target, prop) {
+				if (prop in _window)
+					_window[prop];
+			}
+		});
+		
 		${script.replaceAll(/\b(?<!\[[^\]]*(?=\blet\b[^\]]*\])|{[^}]*(?=\blet\b[^}]*}))let\b/g, 'var')}
 	}`;
 }
-
-export { rewriteUrl, scope };
