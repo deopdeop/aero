97bab79fb3956897079745e218f4a1b6458b8406
desplol <96207058+desplol@users.noreply.github.com>
2022-07-20 14:37:06 -0400

    Delete examples directory

--- examples/basic/static/aero/html.js
+++
@@ -1,8 +0,0 @@
-function rewriteHTML(doc) {
-    return doc
-        .replace(/<meta.*>/g)
-        .replace(/<script.*>(.*)<\/script>/g)
-        .replace(/integrity/g, '_$1');
-}
-
-export { rewriteHTML };
